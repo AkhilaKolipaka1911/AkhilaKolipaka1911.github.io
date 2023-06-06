ba0af29c3f5e7a8895d605de6309b50fec92eb21
AkhilaKolipaka1911 <93595811+AkhilaKolipaka1911@users.noreply.github.com>
2023-06-06 20:35:09 +0530

    updated

--- usercard2.js
+++ usercard2.js
@@ -1,26 +1,20 @@
-let sachininfo{
-    imgurl: "https://images.news18.com/ibnlive/uploads/2022/04/shubman.jpg",
-    name: "Shunman gill",
-    Description: Plays for Gujarat Titans
-}
-let sachinnewinfo = {
-    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9eHf47wsRE1Sfnntlc8XeU8RYMIVKT77IFOqjF2EBcA&usqp=CAU&ec=48600112"
-    name: "Dhoni",
-    description: "orange cap batsman"
-}
-
-let issachinold = true;
-let displayobject;
-
-let flipdata = function(){
-    if(issachinold == true){
-        displayobject = sachinnewinfo;
-        issachinold = false;
+var data1 = {image : "https://www.sportsunfold.com/wp-content/uploads/2022/07/pasted-image-0-18-3.png", name : "Shubman gill", designation : "Crickter"};
+var data2 = {image : "https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2023/62.png", name : "Shubman gill", designation : "Player of Gujarat Titans"};
+var isData1 = true;
+var displayData;
+var changeMyPic = function()
+{
+    if(isData1)
+    {
+        displayData = data2;
+        isData1 = false;
     }
-    else{
-        displayobject = sachinoldinfo;
-        issachinold = true;
+    else
+    {
+        displayData = data1;
+        isData1 = true;
     }
-    document.getElementById("ywc").src=displayobject.imgurl;
-    document.getElementById("ywc-name").innerHTML=displayobject.name;
-    document.getElementById("ywc-des").innerHTML = displayobject.description;
+    document.getElementById("firstimage").src = displayData.image;
+    document.getElementById("myFirstName").innerHTML = displayData.name;
+    document.getElementById("Designation").innerHTML = displayData.designation;
+}
