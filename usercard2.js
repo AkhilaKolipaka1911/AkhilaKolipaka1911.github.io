67af3a5d94968068ff977059293e4d60ee824d33
AkhilaKolipaka1911 <93595811+AkhilaKolipaka1911@users.noreply.github.com>
2023-06-09 00:11:26 +0530

    updated usercard2

--- usercard2.js
+++ usercard2.js
@@ -1,20 +1,21 @@
-var data1 = {image : "https://www.sportsunfold.com/wp-content/uploads/2022/07/pasted-image-0-18-3.png", name : "Shubman gill", designation : "Crickter"};
-var data2 = {image : "https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2023/62.png", name : "Shubman gill", designation : "Player of Gujarat Titans"};
-var isData1 = true;
-var displayData;
-var changeMyPic = function()
+var data1 = {image : "https://www.thesprucepets.com/thmb/FrzaZIcI4aOXxIaRMbomeJUoft8=/2116x0/filters:no_upscale():strip_icc()/Westie-74f76b3f57e643639d67298018374e39.jpg",name : "Chunk" , Breed: "Terrier"};
+var data2 = {image : "https://media-be.chewy.com/wp-content/uploads/2021/06/02102132/Pomeranian_Featured-Image-1024x615.jpg",name : "Dolly" , Breed: "pomeranian"};
+var isdata1 = true;
+var displaydata;
+var changepic = function()
 {
-    if(isData1)
+    if(isdata1)
     {
-        displayData = data2;
-        isData1 = false;
+        displaydata = data2;
+        isdata1=false;
     }
     else
     {
-        displayData = data1;
-        isData1 = true;
+        displaydata = data1;
+        isdata1 = true;
     }
-    document.getElementById("firstimage").src = displayData.image;
-    document.getElementById("myFirstName").innerHTML = displayData.name;
-    document.getElementById("Designation").innerHTML = displayData.designation;
+    document.getElementById("image2").src=displaydata.image;
+    document.getElementById("name2").innerHTML=displaydata.name;
+    document.getElementById("Breed2").innerHTML=displaydata.Breed;
+
 }
